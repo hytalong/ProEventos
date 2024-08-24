c992ebd0afbfed7894c33f0234ef521bf6bc0b76
hytalo <is.oscar@hotmail.com>
2024-08-24 10:33:42 -0300

    Seção 8: Reactive Forms e Layout

--- Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
 
-  ngOnInit() {
+  constructor(public fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.validation();
+  }
+
+  private validation(): void {
+
+    const formOptions: AbstractControlOptions ={
+      validators: ValidatorField.MustMatch('senha', 'confirmeSenha')
+    };
+
+    this.form = this.fb.group({
+      titulo:['', Validators.required],
+      primeiroNome:['', Validators.required] ,
+      ultimoNome:['', Validators.required] ,
+      email:['', [Validators.required, Validators.email]] ,
+      telefone:['', Validators.required],
+      funcao:['', Validators.required],
+      descricao:['', Validators.required],
+      senha:['', [Validators.required, Validators.minLength(6)]] ,
+      confirmeSenha:['', Validators.required] ,
+    }, formOptions);
+  }
+
+  // Conveniente para pegar um FormField apenas com a letra F
+  get f(): any { return this.form.controls; }
+
+  onSubmit(): void {
+
+    // Vai parar aqui se o form estiver inválido
+    if (this.form.invalid) {
+      return;
+    }
+  }
+
+  public resetForm(event: any): void {
+    event.preventDefault();
+    this.form.reset();
   }
 
 }
